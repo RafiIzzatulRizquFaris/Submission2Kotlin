d8fca3c99a214a12cde9b158931c967b5fad156e
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-11-08 05:26:36 +0700

    Changing Launcher Activity

---
+++ app/src/main/java/com/example/submission2kotlin/LeagueActivity.kt
@@ -0,0 +1,12 @@
+package com.example.submission2kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
