e4500da6fe24df6b3aa062321c413cfb03ea3128
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-11-08 06:15:57 +0700

    Applying Anko Layout and adapter RecyclerView

--- app/src/main/java/com/example/submission2kotlin/LeagueActivity.kt
+++ app/src/main/java/com/example/submission2kotlin/LeagueActivity.kt
@@ -1,29 +1,38 @@
 package com.example.submission2kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.submission2kotlin.adapter.AdapterItem
 import com.example.submission2kotlin.model.Item
+import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.startActivity
 
 class LeagueActivity : AppCompatActivity() {
 
+    private lateinit var leagueActivityUI: LeagueActivityUI
     private val items: MutableList<Item> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league)
+        leagueActivityUI = LeagueActivityUI()
+        leagueActivityUI.setContentView(this)
         getData()
+        leagueActivityUI.recyclerView!!.layoutManager = LinearLayoutManager(this)
+        leagueActivityUI.recyclerView!!.adapter = AdapterItem(this, items) {
+            startActivity<DetailLeagueActivity>("detail" to it)
+        }
     }
 
-    private fun getData(){
+    private fun getData() {
         val id = resources.getStringArray(R.array.league_id)
         val name = resources.getStringArray(R.array.league_name)
         val desc = resources.getStringArray(R.array.league_desc)
         val img = resources.obtainTypedArray(R.array.league_image)
 
-        for (i in id.indices){
+        for (i in id.indices) {
             items.add(Item(id[i], name[i], desc[i], img.getResourceId(i, 0)))
         }
-
         img.recycle()
     }
 }
