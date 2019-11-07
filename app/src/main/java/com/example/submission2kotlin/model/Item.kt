a76de32099edfa0a20c88141e7ac71cb5df3768f
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-11-08 05:22:04 +0700

    adding 'Item' Model

---
+++ app/src/main/java/com/example/submission2kotlin/model/Item.kt
@@ -0,0 +1,8 @@
+package com.example.submission2kotlin.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Item (val id: String?, val name: String?, val desc: String?, val image: Int?) :
+    Parcelable
