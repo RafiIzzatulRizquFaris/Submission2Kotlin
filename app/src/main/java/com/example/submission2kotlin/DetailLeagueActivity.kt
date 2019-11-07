4bf9cccf7fc45a264316a74746b6daa7acd15110
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-11-08 06:17:06 +0700

    Add DetailLeagueActivity

---
+++ app/src/main/java/com/example/submission2kotlin/DetailLeagueActivity.kt
@@ -0,0 +1,12 @@
+package com.example.submission2kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailLeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_league)
+    }
+}
