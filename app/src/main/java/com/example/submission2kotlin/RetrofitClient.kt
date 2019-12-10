1c14033f6ec8eaab027c73e622f1454a390bec2c
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-12-10 08:03:39 +0700

    Update RetrofitClient.kt

--- app/src/main/java/com/example/submission2kotlin/RetrofitClient.kt
+++ app/src/main/java/com/example/submission2kotlin/RetrofitClient.kt
@@ -4,16 +4,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitClient {
-    private val baseURL = "https://www.thesportsdb.com/api/v1/json/1/"
-    private var retrofit: Retrofit? = null
-
-    fun getClient(): Retrofit {
-        if (retrofit == null) {
-            retrofit = Retrofit.Builder()
-                .baseUrl(baseURL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+    companion object {
+        private val baseURL = "https://www.thesportsdb.com/api/v1/json/1/"
+        private var retrofit: Retrofit? = null
+        fun getClient(): Retrofit {
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit!!
         }
-        return retrofit!!
     }
 }
