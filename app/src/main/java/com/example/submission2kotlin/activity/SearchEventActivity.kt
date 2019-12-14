4d0b6bf8f26c66ee45d313c837df89ff263b7d1a
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-12-14 23:08:09 +0700

    Add new SearchEventActivity

---
+++ app/src/main/java/com/example/submission2kotlin/activity/SearchEventActivity.kt
@@ -0,0 +1,13 @@
+package com.example.submission2kotlin.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.submission2kotlin.R
+
+class SearchEventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_event)
+    }
+}
