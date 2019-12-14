9a06be96e63552da5a9639e0b0499b88ae916a1c
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-12-14 22:55:00 +0700

    Create RecyclerView Adapter for the Event list

---
+++ app/src/main/java/com/example/submission2kotlin/adapter/AdapterEvent.kt
@@ -0,0 +1,44 @@
+package com.example.submission2kotlin.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.submission2kotlin.R
+import com.example.submission2kotlin.model.Event
+import kotlinx.android.synthetic.main.item_match.view.*
+import com.example.submission2kotlin.adapter.AdapterEvent.ViewHolder as ViewHolder1
+
+class AdapterEvent(
+    private val context: Context,
+    private val matchList: List<Event>,
+    private val listener: (Event) -> Unit
+) :
+    RecyclerView.Adapter<ViewHolder1>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder1 {
+        return ViewHolder1(LayoutInflater.from(context).inflate(R.layout.item_match, parent, false))
+    }
+
+    override fun getItemCount(): Int = matchList.size
+
+    override fun onBindViewHolder(holder: ViewHolder1, position: Int) {
+        holder.itemBind(matchList[position], listener)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun itemBind(event: Event, listener: (Event) -> Unit) {
+            itemView.apply {
+                tv_match_date.text = event.dateEventLocal
+                tv_home_team.text = event.strHomeTeam
+                tv_away_team.text = event.strAwayTeam
+                tv_score_team_home.text = event.intHomeScore
+                tv_score_team_away.text = event.intAwayScore
+            }
+            itemView.setOnClickListener {
+                listener(event)
+            }
+        }
+    }
+}
