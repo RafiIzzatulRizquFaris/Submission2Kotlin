01fb6850e38a5150aedc9e6c779b55c2985524db
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-12-23 19:09:10 +0700

    Update DetailLeagueActivity.kt

--- app/src/main/java/com/example/submission2kotlin/activity/DetailLeagueActivity.kt
+++ app/src/main/java/com/example/submission2kotlin/activity/DetailLeagueActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,44 +13,41 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.bumptech.glide.Glide
 import com.example.submission2kotlin.R
 import com.example.submission2kotlin.adapter.TabFragmentAdapter
+import com.example.submission2kotlin.contract.DetailLeagueContract
 import com.example.submission2kotlin.gone
 import com.example.submission2kotlin.model.Item
-import com.example.submission2kotlin.retrofit.RetrofitClient
-import com.example.submission2kotlin.retrofit.UserService
+import com.example.submission2kotlin.model.League
+import com.example.submission2kotlin.presenter.DetailLeaguePresenter
 import kotlinx.android.synthetic.main.activity_detail_league.*
-import okhttp3.ResponseBody
-import org.json.JSONException
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.io.IOException
 
 
-class DetailLeagueActivity : AppCompatActivity() {
+class DetailLeagueActivity : AppCompatActivity(), DetailLeagueContract.View {
 
-    private lateinit var userService: UserService
+    private lateinit var mPresenter: DetailLeaguePresenter
     private var myIdLeague: String? = null
-    private lateinit var idLeague: String
-    private lateinit var strLeague: String
-    private lateinit var strCountry: String
-    private lateinit var strWebsite: String
-    private lateinit var strFacebook: String
-    private lateinit var strTwitter: String
-    private lateinit var strYoutube: String
-    private lateinit var strDescriptionEN: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_league)
+        mPresenter = DetailLeaguePresenter(this)
         val intentLeague: Item? = intent.getParcelableExtra("detail")
         myIdLeague = intentLeague!!.id!!.toString()
-        val myIntId = myIdLeague!!.toInt()
         Glide.with(this).load(intentLeague.image).into(posterLeague)
-        userService = RetrofitClient.getClient().create(UserService::class.java)
-        getDetailLeague(myIntId)
+        mPresenter.getDetailLeague(myIdLeague!!)
         setSliderFragment()
     }
+
+    override fun setDetailLeague(listLeague: List<League>) {
+        supportActionBar!!.title = listLeague[0].strLeague
+        txtLeagueName.text = listLeague[0].strLeague
+        txtLeagueDescriptionEN.text = listLeague[0].strDescriptionEN
+        ftvFacebook.setOnClickListener { openInBrowser(listLeague[0].strFacebook) }
+        ftvTwitter.setOnClickListener { openInBrowser(listLeague[0].strTwitter) }
+        ftvWebsite.setOnClickListener { openInBrowser(listLeague[0].strWebsite) }
+        ftvYoutube.setOnClickListener { openInBrowser(listLeague[0].strYoutube) }
+        progressBar.gone()
+    }
+
 
     private fun setSliderFragment() {
         val sectionsPagerAdapter: FragmentPagerAdapter =
@@ -65,59 +61,7 @@ class DetailLeagueActivity : AppCompatActivity() {
         tab_layout.setupWithViewPager(view_pager)
     }
 
-    private fun getDetailLeague(id: Int) {
-        val call: Call<ResponseBody?>? = userService.responseDetailLeague(id)
-        call!!.enqueue(object : Callback<ResponseBody?> {
-            override fun onResponse(call: Call<ResponseBody?>, response: Response<ResponseBody?>) {
-                try {
-                    if (response.isSuccessful) {
-                        if (response.body() != null) {
-                            val jsonObject = JSONObject(response.body()!!.string())
-                            val status = jsonObject.toString()
-                            try {
-                                val statusObject = JSONObject(status)
-                                val data = statusObject.getJSONArray("leagues")
-                                for (i in 0 until data.length()) {
-                                    val dataObject = data.getJSONObject(i)
-                                    idLeague = dataObject.getString("idLeague")
-                                    strLeague = dataObject.getString("strLeague")
-                                    strCountry = dataObject.getString("strCountry")
-                                    strWebsite = dataObject.getString("strWebsite")
-                                    strFacebook = dataObject.getString("strFacebook")
-                                    strTwitter = dataObject.getString("strTwitter")
-                                    strYoutube = dataObject.getString("strYoutube")
-                                    strDescriptionEN = dataObject.getString("strDescriptionEN")
-                                }
-                                supportActionBar!!.title = strLeague
-                                txtLeagueName.text = strLeague
-                                txtLeagueDescriptionEN.text = strDescriptionEN
-                                ftvFacebook.setOnClickListener { openInBrowser(strFacebook) }
-                                ftvTwitter.setOnClickListener { openInBrowser(strTwitter) }
-                                ftvWebsite.setOnClickListener { openInBrowser(strWebsite) }
-                                ftvYoutube.setOnClickListener { openInBrowser(strYoutube) }
-                                progressBar.gone()
-                            } catch (e: JSONException) {
-                                e.printStackTrace()
-                                Log.e("JSONEX 2", e.toString())
-                            }
-                        }
-                    }
-                } catch (Ne: JSONException) {
-                    Ne.printStackTrace()
-                    Log.e("JSONEX", Ne.toString())
-                } catch (Oe: IOException) {
-                    Oe.printStackTrace()
-                    Log.e("IONEX", Oe.toString())
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseBody?>, t: Throwable) {
-                Log.e("onFailure", t.toString())
-            }
-        })
-    }
-
-    fun openInBrowser(myURL: String?) {
+    private fun openInBrowser(myURL: String?) {
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://$myURL")))
     }
 
@@ -141,3 +85,56 @@ class DetailLeagueActivity : AppCompatActivity() {
         return true
     }
 }
+
+
+//    private fun getDetailLeague(id: Int) {
+//        val call: Call<ResponseBody?>? = userService.responseDetailLeague(id)
+//        call!!.enqueue(object : Callback<ResponseBody?> {
+//            override fun onResponse(call: Call<ResponseBody?>, response: Response<ResponseBody?>) {
+//                try {
+//                    if (response.isSuccessful) {
+//                        if (response.body() != null) {
+//                            val jsonObject = JSONObject(response.body()!!.string())
+//                            val status = jsonObject.toString()
+//                            try {
+//                                val statusObject = JSONObject(status)
+//                                val data = statusObject.getJSONArray("leagues")
+//                                for (i in 0 until data.length()) {
+//                                    val dataObject = data.getJSONObject(i)
+//                                    idLeague = dataObject.getString("idLeague")
+//                                    strLeague = dataObject.getString("strLeague")
+//                                    strCountry = dataObject.getString("strCountry")
+//                                    strWebsite = dataObject.getString("strWebsite")
+//                                    strFacebook = dataObject.getString("strFacebook")
+//                                    strTwitter = dataObject.getString("strTwitter")
+//                                    strYoutube = dataObject.getString("strYoutube")
+//                                    strDescriptionEN = dataObject.getString("strDescriptionEN")
+//                                }
+//                                supportActionBar!!.title = strLeague
+//                                txtLeagueName.text = strLeague
+//                                txtLeagueDescriptionEN.text = strDescriptionEN
+//                                ftvFacebook.setOnClickListener { openInBrowser(strFacebook) }
+//                                ftvTwitter.setOnClickListener { openInBrowser(strTwitter) }
+//                                ftvWebsite.setOnClickListener { openInBrowser(strWebsite) }
+//                                ftvYoutube.setOnClickListener { openInBrowser(strYoutube) }
+//                                progressBar.gone()
+//                            } catch (e: JSONException) {
+//                                e.printStackTrace()
+//                                Log.e("JSONEX 2", e.toString())
+//                            }
+//                        }
+//                    }
+//                } catch (Ne: JSONException) {
+//                    Ne.printStackTrace()
+//                    Log.e("JSONEX", Ne.toString())
+//                } catch (Oe: IOException) {
+//                    Oe.printStackTrace()
+//                    Log.e("IONEX", Oe.toString())
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<ResponseBody?>, t: Throwable) {
+//                Log.e("onFailure", t.toString())
+//            }
+//        })
+//    }
